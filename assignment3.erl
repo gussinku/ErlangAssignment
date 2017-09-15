e5a09951d49de9139f7537ce885c53e9e6461361
gussinku <gussinku@student.gu.se>
2017-09-15 21:05:09 +0200

    final task for assignment 3 completed

--- assignment3.erl
+++ assignment3.erl
@@ -1,6 +1,6 @@
 -module(assignment3).
 
--export([adj_duplicates/1,%split/2,%
+-export([adj_duplicates/1,split/2,
 	 normalize/1, normalize2/1,
          last/1, find/2,aux_sort/2,sort/1,
          map/2, map2/2, filter/2, filter2/2,
@@ -9,7 +9,7 @@
          dict_new/0, dict_get/2, dict_put/3,
          dict_wellformed/1, dict_map_values/2,
          tree_wellformed/1, tree_make_bfs/1, tree_bind/2,
-         tree_flatten/1, tree_dfs/1, tree_sorted/1, tree_find/2,max_norma/1
+         tree_flatten/1, tree_dfs/1, tree_sorted/1,reverse/1, tree_find/2,max_norma/1
         ]).
 
 
@@ -20,13 +20,22 @@ adj_duplicates([A,A|Tail]) -> [A|adj_duplicates([A|Tail])];
 adj_duplicates([_Head|Tail]) -> adj_duplicates(Tail).
 
 
-%split(0,L) -> {[],L};
-%split(_,[]] -> {[],[]};
-%split(N,X|Xs) -> 
-  %{X,Y} = split(N -1,Xs),{[X|Y],Z}.
+
+%
+split(N, L) ->  split(N, L, []).
+
+split(N, L, Acc) when N < 0 -> {L, Acc};
+split(_N, [], Acc) -> {reverse(Acc), []};
+split(0, T, Acc) -> {reverse(Acc), T};
+split(N, [H|T], Acc) when N > 0 -> split(N-1, T, [H|Acc]).
+%Tail recursive function of reverse.
+reverse(L) -> reverse(L,[]).
+reverse([],R) -> R;
+reverse([H|T],R) -> reverse(T,[H|R]).
 
 
 
+%
 normalize(L) -> [ X / max_norma(L)|| X <- L].
 max_norma(L) -> max_norma(L ,0). 
 max_norma([],N) -> N;
@@ -50,48 +59,79 @@ find(Predicate, [H|T]) ->
 end;
 find(_Predicate , []) -> not_found.  
 
-
+%sort a list of elements by taking them one at a time and insert them into an already sorted list
 sort(L) -> sort(L,[]).
 sort([],S) -> S;
 sort([H|T],S) -> sort(T, aux_sort(H , S)).  
 
 aux_sort(X,[]) -> [X];
-aux_sort(X,[H]) when X =< H -> [X|[H]];
+aux_sort(X,Z=[H|_]) when X =< H -> [X|Z];
 aux_sort(X,[H|T]) -> [H|aux_sort(X, T)].
  
 
     
 
 
-%
+%Mapping lists to the 
     map(F, [H|T]) -> [F(H)| map(F, T)];
     map(_F, []) -> [].
 %List comprehension of a map function
  map2(F, L) -> [ F(X)  ||  X <- L ].
 
-filter(_, _) -> not_implemented.
+%using recursion
+ filter(F, [H|T]) ->
+  case F(H) of
+      true  -> [H|filter(F, T)];
+      false -> filter(F, T)
+  end;
+filter(_F, []) -> [].
 
-filter2(_, _) -> not_implemented.
+%using list comprehension. P is a predicate which means a function takes a value and returns boolean value
+% instead of X== P, write P(X) == true.
+filter2(F, L) -> [ X || X <- L, F(X)] .
 
 
 % 3. Digitify a number 
+%Takes a value and returns a list of numbers.
+digitize(0) -> 1/0;
+digitize(N) -> reverse(aux_digit(N)).
+   
+aux_digit(N) when N >= 0 -> 
+        case N of  
+            0 -> [];                                    
+            _ -> [N rem 10 | aux_digit(N div 10)]
+        end.
 
-digitize(_) -> not_implemented.
 
 
 % 4. Happy numbers
+%puts a digitise number and calculates it same and then compares the value with true or false if 1 or 4.
+is_happy(1) -> true;
+is_happy(4) -> false;
+is_happy(N) -> D = digitize(N),is_happy(calculate(D,0)).
+calculate([], Total)-> Total;
+calculate([H | T], Total) -> calculate(T, Total + (H * H)).
 
-is_happy(_) -> not_implemented.
 
-all_happy(_, _) -> not_implemented.
-
+%Uses the already implemeneted interval ,is_happy function and returns predate true or false of numbers between A and B
+all_happy(N1, N2) -> [X || X <- interval(N1,N2), is_happy(X)].
+interval(A, B) ->  interval(A, B, []).
+interval(A,B, L) when A > B -> L;
+interval(A,B,L) -> interval(A, B-1, [B|L]).
 
 % 5. Expressions
 
-expr_eval(_) -> not_implemented.
+expr_eval({num, N}) -> N;
+expr_eval({mul, N1, N2}) -> expr_eval(N1) * expr_eval(N2);
+expr_eval({plus, N1, N2}) -> expr_eval(N1) + expr_eval(N2);
+expr_eval({minus, N1, N2}) -> expr_eval(N1) - expr_eval(N2).
 
-expr_print(_) -> not_implemented.
-
+%
+expr_print({num, 0}) -> [$0 + 0];
+expr_print({num, N}) -> [$0 + X|| X <- digitize(N)];
+expr_print({mul, N1, N2}) -> lists:append("(", lists:append(expr_print(N1), lists:append("*",lists:append(expr_print(N2), ")"))));
+expr_print({plus, N1, N2}) -> lists:append("(", lists:append(expr_print(N1), lists:append("+",lists:append(expr_print(N2), ")"))));
+expr_print({minus, N1, N2}) -> lists:append("(", lists:append(expr_print(N1), lists:append("-",lists:append(expr_print(N2), ")")))).
 
 % 6. Dictionary
 
