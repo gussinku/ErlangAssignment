8e7ebc4f1322f9c812787e3e493b491fefa4bc54
gussinku <gussinku@student.gu.se>
2017-09-20 16:39:55 +0200

    question 1 of 3 task completd

--- assignment4_counter.erl
+++ assignment4_counter.erl
@@ -2,16 +2,61 @@
 
 -export([start/0, init/0, incr/1, fetch/1, reset/1, stop/1
         ]).
+-export([loop/1]).
 
 
-start() -> not_implemented.
+start() -> spawn(assignment4_counter,init,[]).
 
-init() -> not_implemented.
+init() -> io:format("starting ...~n"),
+        PID = self(),
+          register(assignment4_counter,PID),
+          loop({0,PID}).
+      
 
-incr(_) -> not_implemented.
+loop({Count, S}) ->
+   receive
+        {incr,Pid} ->
+                 Pid ! done, loop({Count+1, S});
 
-fetch(_) -> not_implemented.
+        {fetch,Pid} ->
+                   Pid ! Count,
+                     loop({Count,S});
+        
+         {reset, Pid} ->
+                Pid ! done,
+                  loop({0, S});
 
-reset(_) -> not_implemented.
+                  {stop,Pid} -> 
+                          Pid ! done
 
-stop(_) -> not_implemented.
+        end. 
+
+incr(S) ->
+         S ! {incr, self()},
+         receive
+                 done -> done
+                after 500 -> process_might_be_stopped
+                 end.
+
+fetch(S) -> 
+   S ! {fetch, self()},
+       receive
+           N -> N
+        after 500 -> process_might_be_stopped
+        end.
+
+reset(S) ->  
+    S ! {reset, self()},
+   receive
+        done -> ok
+after 500 -> process_might_be_stopped
+    end.
+
+stop(S) ->    
+S ! {stop, self()},
+   receive
+         done -> ok
+        after 500 -> process_might_be_stopped
+
+ end.
+      
