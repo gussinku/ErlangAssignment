df0b6289481e7db6c9d22f04d3f99a86fdce4cad
gussinku <gussinku@student.gu.se>
2017-09-22 15:05:53 +0200

    question 2 or 3 soultiomsn added

--- assignment4.erl
+++ assignment4.erl
@@ -1,7 +1,7 @@
 -module(assignment4).
 
--export([task/1, dist_task/1, pmap/2, faulty_task/1
-        ]).
+-export([task/1, dist_task/1,pmap/2,faulty_task/1]).
+
 
 
 %%
@@ -28,11 +28,27 @@ faulty_task(N) ->
     end.
 %% End of no-change area
 
-dist_task(_) ->
-  not_implemented.
+%This function given a list of numbers, spawn a process for each computation and collect all the computation results
+dist_task(L) ->
+        Parent = self(),
+        Pids = [spawn(fun() ->
+                   Parent ! {self(),task(X)}
+                end) || X <- L],
+                [receive {Pid,Res} -> Res end || Pid <- Pids].
 
-pmap(_, _) ->
-  not_implemented.
+
+ %catches errors and include them in the map as normal values to the user
+pmap(F, L) ->
+        await1(spawn_jobs1(F,L)). 
+        spawn_jobs1(F, L) ->
+           Parent = self(),
+           [spawn(fun() ->
+        Parent ! {self(), catch F(X)} end) || X <- L].
+%monitors the pids and match them to specific ref
+        await1(Pids) ->
+        [receive {Pid,Res} -> Res end || Pid <- Pids].
+
+
 
 
 %%
--- assignment4_store.erl
+++ assignment4_store.erl
@@ -1,23 +1,68 @@
 -module(assignment4_store).
-
 -export([start/0, init/0, stop/0, store/2, fetch/1, flush/0
         ]).
+start() -> 
+        case whereis(sts) of
+          undefined ->
+             Pid = spawn(fun() -> init() end),
+             register(sts, Pid),
+             {ok, Pid};
+          Pid  ->
+             {ok,Pid}
+     end.
+   
 
-start() ->
-  not_implemented.
+ init() -> io:format("Starting... ~n"),
+       loop([]).
 
+   
+      loop(Dict) ->
+          io:format("dict is ... ~n"),
+          receive
+                  {store, K, V, Pid} ->
+                    case proplists:is_defined(K, Dict) of
+                      false     -> Dict2 = [{K, V}| Dict], Pid ! {value, V};
+                      true       -> Dict2 = Dict, Pid ! {ok,no_value}
+                    end,
+                    loop(Dict2);
+                  {fetch, K, Pid} ->
+                    case proplists:is_defined(K, Dict) of
+                      true     -> [{_K2, V}] = proplists:lookup_all(K, Dict),Pid ! {value, V};
+                      false       -> Pid ! {error, not_found}
+                    end,
+                    loop(Dict);
+
+                  {stop, Pid} ->  
+                    Pid ! done;
+
+                  {flush,Pid} ->
+                  Pid ! ok,
+                  loop([])
+                end.
 stop() ->
-  not_implemented.
+  sts ! {stop, self()},
+  receive
+    done -> ok
+after 500 -> process_might_be_stopped
+end.
+store(K, V) ->
+  sts ! {store, K, V, self()},
+receive
+  {error, not_found} -> {error, not_found};
+  {value, V} -> {value, V}
+end.
+fetch(K) ->
+  sts ! {fetch, K, self()},
+  receive
+    {value, V} -> {value, V};
+    {error, not_found} -> {error, not_found}
+  end.
 
-store(_, _) ->
-  not_implemented.
-
-fetch(_) ->
-  not_implemented.
-
-flush() ->
-  not_implemented.
-
-init() ->
-  not_implemented.
-
+  flush() ->
+      receive
+          M -> 
+              io:format("Shell got ~p~n",[M]),
+              flush()
+      after 0 ->
+          ok
+      end.
